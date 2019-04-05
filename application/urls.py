c4b93e4d073619182b74486197e1412f56359169
Orlando Camarillo <orlandosalvadorcamarillomoreno@gmail.com>
2019-04-05 17:26:32 -0600

    set redirect url to logout

--- application/urls.py
+++ application/urls.py
@@ -32,5 +32,19 @@ urlpatterns = [
     # REST API
     path('api/space/filter/', views.filter_spaces, name='filter_spaces'),
 
+    #Users
+    path(r'admin/', admin.site.urls),
+    
+    path('signup', views.signup, name='signup'),
+    re_path(r'login/', auth_views.LoginView.as_view(), name='login'),
+    re_path(r'logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('reset_password',views.password_reset,name='password_reset'),
+    path('reset_done',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    url(r'password_reset_confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',views.password_reset_confirm,name='password_reset_confirm'),
+    #activation email
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
+
+
     path(r'admin/', admin.site.urls),
 ]
